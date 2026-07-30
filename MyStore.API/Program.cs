f86c2d85a96e29393169c22bae80afcefd4e2088
hatem-hamdan <hatemhamdan392@gmail.com>
2026-07-30 08:34:40 +0300

    Fix CORS origins in Program.cs
    Removed duplicate CORS origin entry and fixed formatting.

--- MyStore.API/Program.cs
+++ MyStore.API/Program.cs
@@ -66,8 +66,7 @@ builder.Services.AddCors(options =>
                 "http://localhost:5215",
                 "https://localhost:5173",
                 "https://my-store5.vercel.app",
-                            "https://e-commerce-frontend-v2-4rzp.vercel.app,
-       "https://e-commerce-frontend-v2-ijfm.vercel.app"
+                            "https://e-commerce-frontend-v2-4rzp.vercel.app
 
 
             )
