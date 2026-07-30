e2ee6aa8432416bad02369a79373893632b2e6cb
hatem-hamdan <hatemhamdan392@gmail.com>
2026-07-30 10:10:23 +0300

    Update OrderCreateDto.cs

--- MyStore.DataAccess/DTOs/OrderCreateDto.cs
+++ MyStore.DataAccess/DTOs/OrderCreateDto.cs
@@ -17,5 +17,7 @@
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
+                public string Color { get; set; }
+
     }
 }
